11361501368b706718f1e4b1447e7c2aa90dcf09
David Scott <david.scott@cloudscaling.com>
2013-11-21 11:21:08 -0800

    Initial commit of jquery.repeat-element.js

---
+++ jquery.repeat-element.js
@@ -0,0 +1,18 @@
+/*
+ * jQuery Repeat Element Plugin v0.0.1
+ * https://github.com/dscottcs/jquery-repeat-element
+ *
+ * This document is licensed as free software under the terms of the
+ * MIT License: http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright (c) 2013 by dscottcs (David Scott).
+ */
+
+(function($){
+    $.extend({
+        repeat-element: function(proto, opts) {
+            var repeat_el;
+            return repeat_el;
+        }
+    });
+})(jQuery);
