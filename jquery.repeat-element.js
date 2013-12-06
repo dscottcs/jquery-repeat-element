972724b93f6bc5ff41ec18173509e2b69a2fb92c
David Scott <david.scott@cloudscaling.com>
2013-12-06 10:34:45 -0800

    Make 'jq' element the parent of prototype, cache in data hash.
    Also bump version to 0.0.2

--- jquery.repeat-element.js
+++ jquery.repeat-element.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Repeat Element Plugin v0.0.1
+ * jQuery Repeat Element Plugin v0.0.2
  * https://github.com/dscottcs/jquery-repeat-element
  *
  * This document is licensed as free software under the terms of the
@@ -9,13 +9,12 @@
  */
 
 (function($){
-  var version = 'jQuery.repeat-element 0.0.1';
+  var version = 'jQuery.repeat-element 0.0.2';
 
-  // By default we bind data to the innerText attribute of the element
-  // More sophisticated bindings (including form element binding) will require a custom bindFun
+  // By default we bind data based on class names on subelements of the repeated element
+  // More sophisticated bindings (including form element binding) will require a custom bindFn
 
-  var defaultBind = function(element, dataObj) {
-    var el = $(element);
+  var defaultBind = function(el, dataObj) {
     $.each(dataObj, function(key, value) {
       var subEl = el.find('.' + key);
       if (subEl) {
@@ -26,29 +25,40 @@
 
   // Generate repeats, bind data
 
-  var bindToData = function(data, proto, bindFunc, idAttr) {
-    var protoClass = proto.attr('class') || 'repeat';
-    var parent = proto.parent();
-    parent.empty();
-    $.each(data, function(index, dataObj) {
+  var bindToData = function(jq, bindData, bindFn, idAttr) {
+    if (bindData.length == 0) {
+      return;
+    }
+    // Pull proto from jq object cache;
+    var proto = jq.data('proto');
+    var protoClass = proto.attr('id');
+    // Get rid of repeats from previous execution if any
+    jq.empty();
+    // If there are more than one object, add repeats and bind them
+    $.each(bindData, function(index, dataObj) {
       var id = dataObj[idAttr];
       var copy = proto.clone();
       copy.attr('id', protoClass + '_' + id);
-      parent.append(copy);
-      bindFunc(copy, dataObj);
-    })
+      copy.addClass(protoClass);
+      jq.append(copy);
+      bindFn(copy, dataObj);
+    });
   };
 
   // Generate repeats, provide unique ID for each, do not bind any data
 
-  var generateRaw = function(rawNum, proto) {
-    var protoClass = proto.attr('class') || 'repeat';
-    var parent = proto.parent();
-    parent.empty();
+  var generateRaw = function(jq, rawNum) {
+    // Pull proto from jq object cache;
+    var proto = jq.data('proto');
+    var protoClass = proto.attr('id');
+    // Get rid of repeats from previous execution, if any
+    jq.empty();
+    // If we need repeats add them and set their IDs
     for (var i = 0; i < rawNum; i++) {
       var copy = proto.clone();
       copy.attr('id', protoClass + '_' + i);
-      parent.append(copy);
+      copy.addClass(protoClass);
+      jq.append(copy);
     }
   };
 
@@ -66,41 +76,42 @@
     //
     // If both 'data' and 'raw' are defined, 'raw' is ignored.
 
-    var data = opts.data;
+    var bindData = opts.bindData;
     var raw = opts.raw;
-    if (data === undefined && raw === undefined) {
-      throw '"data" object or numeric "raw" value required';
+    if (bindData === undefined && raw === undefined) {
+      throw '"bindData" object or numeric "raw" value required';
     }
     if (raw && ((! $.isNumeric(raw)) || (raw < 0))) {
       throw '"raw" option must be a nonnegative integer';
     }
-    if (raw === undefined && ! $.isArray(data)) {
-      throw '"data" option must be a list'
+    if (raw === undefined && ! $.isArray(bindData)) {
+      throw '"bindData" option must be a list'
     }
-    var protoClass = this.attr('class');
-    if (protoClass && protoClass.indexOf(' ') > -1) {
-      throw 'Prototype object must have only one class name';
+    // If object has been used before, the prototype will be cached in the 'data' hash
+    // Otherwise we clone the prototype subelement and cache it
+    var proto = this.data('proto');
+    if (! proto) {
+      var kids = this.children();
+      if (! kids) {
+        throw 'Element must include a prototype for repeat';
+      }
+      var firstChild = kids.first();
+      this.data('proto', firstChild.clone());
     }
 
+    // Use the 'idAttr' option value as ID if present, otherwise 'id'
     var idAttr = (!!opts.idAttr) ? opts.idAttr : 'id';
 
-    // A bit of jQuery wizardry here: we remove the prototype from the parent
-    // and insert a bunch of clones (optionally bound to data).
-    // But 'this' points to the prototype that goes away when we do the generation/data binding
-    // So we keep a reference to the prototype's parent before doing the transformation.
-    // The parent is always well defined, and is what we're most interested in anyway
-    // The return value becomes the set of repeated elements (the parent's children).
-
-    var parent = this.parent();
-    if ($.isArray(data)) {
-      var bindFunc = ('function' == typeof opts.bindFunc) ? opts.bindFund : defaultBind;
-      bindToData(data, this, bindFunc, idAttr);
+    // 'repeat' phase: bind to a data array or generate raw, as befits
+    if ($.isArray(bindData)) {
+      var bindFn = ('function' == typeof opts.bindFn) ? opts.bindFn : defaultBind;
+      bindToData(this, bindData, bindFn, idAttr);
     }
     else {
-      generateRaw(raw, this);
+      generateRaw(this, raw);
     }
 
-    var repeated = parent.find('.' + protoClass);
-    return repeated;
+    // That's it
+    return this;
   }
 })(jQuery);
